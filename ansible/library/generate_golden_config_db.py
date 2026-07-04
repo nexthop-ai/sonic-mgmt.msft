eeccc7de3e9ee3301a60cf9692e0a348f131dfbb
purush-nexthop <purush@nexthop.ai>
2026-07-04 16:32:33 +0000

    Added LT2-min topo with 6 UT2 and 2 T1 neighbors
    Signed-off-by: purush-nexthop <purush@nexthop.ai>

--- ansible/library/generate_golden_config_db.py
+++ ansible/library/generate_golden_config_db.py
@@ -811,7 +811,7 @@ class GenerateGoldenConfigDBModule(object):
         """
         Generate golden_config for FT2 to enable FEC and set BGP confed.
         """
-        SUPPORTED_TOPO = ["ft2-64", "ft2-16", "lt2-p32o64", "lt2-o128", "lt2-o128-d110u14",
+        SUPPORTED_TOPO = ["lt2-min", "ft2-64", "ft2-16", "lt2-p32o64", "lt2-o128", "lt2-o128-d110u14",
                           "ft2-o128", "lt2-o256-u32d224", "lt2-u32d128"]
         if self.topo_name not in SUPPORTED_TOPO:
             return "{}"
